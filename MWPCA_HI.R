c829e694f49dad08dc116a30bd3c10d086c24a7e
Héloïse V <46787261+Loryana@users.noreply.github.com>
2022-03-16 17:21:13 +0100

    Update MWPCA_HI.R

--- MWPCA_HI.R
+++ MWPCA_HI.R
@@ -2,15 +2,14 @@
 rm(list=ls())
 setwd("~/Thèse/Script/MWPCA fonction")
 
-source("Movingwindowpca_modif_HI2021.R")
+source("Movingwindowpca_modif_HI2021_distrib.R")
 source("asd_read.R")
 
 library(rnirs)
 library(ggplot2)
+library(R.matlab)
 
 file_spectra="~/Thèse/Manip HI2021/Feuilles_test/Spectres 100"
-# g_mwpca="~/Thèse/Manip HI2021/Feuilles_test/Spectres R VNIR/MWPCA"
-# g_resultat="~/Thèse/Manip HI2021/Feuilles_test/Spectres R VNIR/Résultat"
 
 test_file <- function(path_file){
   if(file.exists(path_file)==T){
@@ -22,29 +21,37 @@ test_file <- function(path_file){
 
 test_file(file_spectra)
 
-genotyp='19_G'
-num_feuil=1
+ind='19_G'
+num_feuil=2
 num_spectra=10000
-repetition=10
+repetition=3
 wchoix=c(1)
 date=c('20210311','20210312','20210316','20210317','20210318','20210319',
        '20210320','20210322','20210323','20210324','20210325','20210326',
        '20210327','20210328','20210329','20210330','20210331','20210401')
 
+path_cwd <- "C:/Users/heloise.villesseche/Documents/Thèse/Manip HI2021/Feuilles_test/Output VNIR"
+path_cwd
+setwd(path_cwd)
+filename_tot=dir(path = path_cwd)
+
+filename=c()
+for(tot in 1:length(filename_tot)){
+  if(substr(filename_tot[tot],10,13)==ind & substr(filename_tot[tot],56,56)==num_feuil){
+    filename=c(filename,filename_tot[tot])
+  }
+}
+
+
 T1<-Sys.time()
 result=vector("list",repetition)
 for(uti in 1:repetition){
   for(w in 1:length(wchoix)){
-     
-    
-    path_cwd <- "C:/Users/heloise.villesseche/Documents/Thèse/Manip HI2021/Feuilles_test/Output VNIR"
-    path_cwd
-    setwd(path_cwd)
-    filename=dir(path = path_cwd)
     
     chemin='C:/Users/heloise.villesseche/Documents/Thèse/Manip HI2021/Feuilles_test/Spectres 100'
     
     for(i in 1:length(filename)){
+      
       setwd(path_cwd)
       data=readMat(filename[i])
       samp=sample(c(1:nrow(data$matr)),num_spectra)
@@ -68,13 +75,13 @@ for(uti in 1:repetition){
     
     setwd(file_spectra)
     for(i in 1:length(date)){
-      filename2=paste(date[i],'_',genotyp,'_',num_feuil,'_',num_spectra,'.RData',sep='')
+      filename2=paste(date[i],'_',ind,'_',num_feuil,'_',num_spectra,'.RData',sep='')
       load(filename2)
       datagen[c((1+(num_spectra*(i-1))):(num_spectra*i)),]=data_rand
       date_gen=c(date_gen,rep(i,num_spectra))
     }
     
-    if(nrow(data_rand)>ncol(data_rand)){
+    if((2*nrow(data_rand))>ncol(data_rand)){
       ncomposante=ncol(data_rand)-1
     }else{
       ncomposante=(2*nrow(data_rand))-1
@@ -87,45 +94,31 @@ for(uti in 1:repetition){
                   date_gen_test=date_gen,
                   datemax=max(date_gen),
                   rnirmeth=TRUE)
-    
-    # hist(oh[[1]],breaks=15,main=paste(genotyp,": Taille de la fenêtre =",wchoix + 1))
-    # #dev.print(device = png, file = paste(genotyp[g],"_hist_T-w_",h+1,'_composante_',ncomposante,".png",sep=''), width = 600)
-    # hist(oh[[2]],breaks=15,main=paste(genotyp,": Taille de la fenêtre =",wchoix + 1))
-    # #dev.print(device = png, file = paste(genotyp[g],"_hist_Q_w_",h+1,'_composante_',ncomposante,".png",sep=''), width = 600)
-    # 
-    # supra=c()
-    # for(k in 1:length(boxplot(oh[[1]])$out)){
-    #   supra=c(supra,date_plot[which(oh[[1]]==boxplot(oh[[1]])$out[k])+wchoix])
-    # }
-    # mana=c()
-    # for(l in 1:length(boxplot(oh[[2]])$out)){
-    #   mana=c(mana,date_plot[which(oh[[2]]==boxplot(oh[[2]])$out[l])+wchoix])
-    # }
-    
-    # bidule=data.frame(date=mm$degresjour[-c(1:(wchoix+1))],Tcarre=oh[[1]])
-    # machin=data.frame(Qstat=oh[[2]])
-    # 
-    # df <- cbind(bidule,machin)
-    # 
-    # p <- ggplot()
-    # p <- p + geom_point(data=df,aes(x=date, y=Tcarre,color="#F8766D"),size=4)
-    # p <- p + geom_line(data=df,aes(x=date, y=Tcarre,color="#F8766D"))
-    # p <- p + geom_point(data=df,aes(x=date, y=Qstat,color="#00BFC4"),size=4)
-    # p <- p + geom_line(data=df,aes(x=date, y=Qstat,color="#00BFC4"))
-    # p <- p + ggtitle(paste(genotyp[g],": Taille de la fenêtre =",h + 1))
-    # p <- p + xlab("Degrés jour")
-    # p <- p + ylab("Statistiques")
-    # p <- p + scale_colour_manual(name = "Statistiques", values = c("#F8766D", "#00BFC4"),labels=c("T²","Q"))
-    # p <- p + theme(plot.title = element_text(hjust = 0.5))
-    # p
-    #ggsave(paste(genotyp[g],"_w_",h+1,'_composante_',ncomposante,".png",sep=''), width = 8, height = 6)
-    
   }
   result[[uti]] = oh
+  
+  # chemfinal=paste('C:/Users/heloise.villesseche/Documents/Thèse/Manip HI2021/Feuilles_test/Résultat VNIR',
+  #                 '/',ind,sep='')
+  # test_file(chemfinal)
+  # setwd(chemfinal)
+  # nomplot=paste(ind,'_',num_feuil,'_',uti,'_','T².jpg',sep='')
+  # jpeg(nomplot, width = 900, height = 500)
+  # plot(strptime(date[-c(1,2)],"%Y%m%d"),rowMeans(result[[uti]]),type='l',
+  #      main=paste(ind,'feuille',num_feuil,'T²'),
+  #      xlab="Temps",ylab="T²")
+  # dev.off()
+  # nomplot2=paste(ind,'_',num_feuil,'_',uti,'_','SD.jpg',sep='')
+  # jpeg(nomplot2, width = 900, height = 500)
+  # plot(strptime(date[-c(1,2)],"%Y%m%d"),c(apply(result[[uti]],1,sd)),type='l',
+  #      main=paste(ind,'feuille',num_feuil,'Ecart-type'),
+  #      xlab='Temps',ylab='Ecart-type')
+  # dev.off()
+  
+  
   T2<-Sys.time() 
 }
 
-setwd("~/Thèse/Manip HI2021/Feuilles_test/Résultat")
-save(result,file=paste(num_spectra,'.RData',sep=''))
+setwd("~/Thèse/Manip HI2021/Feuilles_test/Résultat_distrib")
+#save(result,file=paste(num_spectra,'_distrib_nco_',ncomposante,'.RData',sep=''))
 
-Tdiff= difftime(T1, T2) 
+Tdiff= difftime(T2, T1) 
